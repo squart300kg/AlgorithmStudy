dda5ade6e017a008a4ebb341bf2403e431fca81e
SSY <57611838+squart300kg@users.noreply.github.com>
2022-12-27 01:28:39 +0900

    [level 0] Title: 문자열 밀기, Time: 11.55 ms, Memory: 60.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120921. 문자열 밀기/README.md
+++ 프로그래머스/lv0/120921. 문자열 밀기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 문자열 밀기 - 120921 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120921) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120921?language=kotlin) 
 
 ### 성능 요약
 
-메모리: 60.6 MB, 시간: 9.08 ms
+메모리: 60.6 MB, 시간: 11.55 ms
 
 ### 구분
 
--- 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.kt
+++ 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.kt
@@ -1,14 +1,11 @@
 class Solution {
     fun solution(startString: String, targetString: String): Int {
-        var answer = 0
-
         val tempStartString = StringBuilder(startString)
         repeat (startString.length) {
             if ("$tempStartString" != targetString) {
-                answer++
                 tempStartString.insert(0, tempStartString.takeLast(1)).deleteAt(tempStartString.length - 1)
             }
-            else return answer
+            else return it
         }
         return -1
     }
